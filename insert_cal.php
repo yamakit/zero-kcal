a7681ebbe474f553bec4e6f7fccc9451664fc9ed
yamakit <18439@toba.kosen-ac.jp>
2021-02-27 16:51:30 +0900

    大まかなページ構成

--- insert_cal.php
+++ insert_cal.php
@@ -41,3 +41,21 @@
   
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- タイトルをつけてボタンを見た目整える -->
+    <input type=button onclick="location.href='result_cal.html'" value=Upload>
+</body>
+
+</html>
