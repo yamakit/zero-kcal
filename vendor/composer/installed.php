dc52f80a1053ea590dd7d27e13f9b81b290220ab
yamakit <18439@toba.kosen-ac.jp>
2021-02-27 15:23:55 +0900

    db接続の追加

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fda14e2d20aba2c1efc05d3c0c9318e716be110d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fda14e2d20aba2c1efc05d3c0c9318e716be110d',
+    ),
+  ),
+);
