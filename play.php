6d12a2947992e4d12d45fd32aa717d1d6714578c
yamakit <18439@toba.kosen-ac.jp>
2021-03-01 16:05:35 +0900

    ajax通信を追加

--- play.php
+++ play.php
@@ -22,6 +22,7 @@ foreach($age_data as $age_data_val){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
